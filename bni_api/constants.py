66363a3d5327c336b43883e88656f50349a0e9e6
loncat <me@lcat.dev>
2019-06-30 10:15:18 +0800

    Added constansts and utils

---
+++ bni_api/constants.py
@@ -0,0 +1,6 @@
+BNI_IBANK_URL = 'https://ibank.bni.co.id/MBAWeb/FMB'
+REQ_USER_AGENT = ('Mozilla/5.0 (Linux; U; Android 2.2)' +
+                  ' AppleWebKit/533.1 (KHTML, like Gecko)' +
+                  ' Version/4.0 Mobile Safari/533.1')
+SESSION_TTL = 5 * 60  # 5 minutes
+DATE_FORMAT = r'%d-%b-%Y'
